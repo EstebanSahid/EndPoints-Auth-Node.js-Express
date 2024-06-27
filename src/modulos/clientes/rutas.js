134de5ff0b1f4ddc3cbe36f1eb478f1974287bb8
Esteban Sahid <esteban_ct28@hotmail.com>
2024-06-27 18:58:30 -0500

    cambio a async a la consulta

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -5,12 +5,9 @@ const controlador = require('./controlador');
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    const todos = controlador.todos()
-    .then((items) => {
-        respuestas.success(req, res, items, 200)
-    })
-    
+router.get('/', async function (req, res) {
+    const items = await controlador.todos()
+    respuestas.success(req, res, items, 200)
 });
 
 module.exports = router;
