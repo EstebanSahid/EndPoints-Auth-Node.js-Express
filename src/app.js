52a7b3e83477854990ae0344d052d458f92ff5b3
Esteban Sahid <esteban_ct28@hotmail.com>
2024-06-27 19:05:31 -0500

    middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const config = require('./config');
 
 const clientes = require('./modulos/clientes/rutas');
 
 const app = express();
+
+// Middleware
+app.use(morgan('dev'))
 
 // Configuración de Puerto
 app.set('port', config.app.port);
